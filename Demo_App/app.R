2eb25c8831f1e819bf0349abeaf9e51ce75770ad
noahreimers <43415155+noahreimers@users.noreply.github.com>
2018-12-05 00:06:08 -0500

    final commit

--- Demo_App/app.R
+++ Demo_App/app.R
@@ -11,10 +11,15 @@ library(shiny)
 library(devtools)
 library(plotly)
 
+
+## I start by loading in my two different datasets that I will need for the plots.
+
 pgroups <- read_rds("p_groups_records.rds")
 draft_value <- read_rds("draft_value_ratings.rds")
 
-## I am defining the choices for the first plot
+
+## I am defining the choices for the first plot, by writing out each of the variables that you will be able to select in the 
+## dropdown area.
 
 x1_choices <- c("QB Ratings" = "qb_sum",
                 "RB Ratings" = "rb_sum",
@@ -30,6 +35,8 @@ y1_choices <- c("Offense Rating" = "Overall_Offense",
                 "Winning Percentage" = "Pct",
                 "Pythagorean Expectation" = "Pyt_Exp")
 
+## Here I am doing the same thing for the second plot.
+
 x2_choices <- c("Position Group Yearly Rating" = "team_total",
                 "Draft Pick Strength Index" = "draft_picks",
                 "Value derived from those Picks (PFR)" = "value",
@@ -41,6 +48,10 @@ y2_choices <- c("Position Group Yearly Rating" = "team_total",
                 "Value derived from those Picks (PFR)" = "value",
                 "Value per Draft Pick Strength index" = "value_per",
                 "Win Percentage over Period (2008-2012)" = "Win_Pct")
+
+## Here I entered all the choices for when you filter by position. I also entered all the team names because I wanted to 
+## be able to filter by both position and team separately, but the only way I could figure out how to do it was if they did them
+## together and that yielded just one data point. Something to figure out.
 
 p2_choices <- c("QB" = "QB",
                 "RB" = "RB",
@@ -83,7 +94,7 @@ teams <- c("Arizona Cardinals" = "ARZ",
            "Tampa Bay Buccaneers" = "TB",
            "Tennessee Titans" = "TEN",
            "Washington Redskins" = "WAS")
-all <- ("All")
+
 
 # Define UI for application that looks at the individual player ratings from Pro Football Focus and how they lead to success.
 
@@ -108,13 +119,29 @@ ui <- navbarPage(
                      selected = "Pct")
       ),
       
-      # Show a plot of the generated distribution
+      ## So here I am entering in the column where the information about the data point that is clicked will show up. I haven't
+      ## fully figured out how best to do this, but this provides a base-level fix of displaying the dataset in a very ugly way.
+      ## Still something that needs work. I want to be able to click or hover over each datapoint and see what team it is, that
+      ## way you might be able to think through why it looks that way, or who the outliers are. I did this point selection in
+      ## each of the plots.
+      
       mainPanel(
-         plotlyOutput("Plot")
-      )
+        column(width = 9,
+               plotOutput("Plot", height = 300,
+                            click = "Plot_click")
+      ),
+      column(width = 7, 
+             h4("Points near click"),
+             verbatimTextOutput("click_info")
+             )
               )
             )
+            )
   ),
+  
+  ## Here I am reading in the second tab and describing what each of the variables means. I tinkered with the explanations of the
+  ## variables a bit, and thought this was the best. Could need some more alterations.
+  
   tabPanel("Ratings and the NFL Draft",
            fluidPage(
              titlePanel("PFF Ratings, Draft Picks and Value Derived from those picks"),
@@ -143,8 +170,19 @@ ui <- navbarPage(
                              selected = "QB"
                              )
              ),
+             
+             ## Here is where I am doing the same procedure as above to make it possible to see what team each data point 
+             ## represents. This is particularly interesting for this plot because it shows the team that drafted and from there
+             ## a big NFL fan like myself can deduce who the draft pick was on.
+             
              mainPanel(
-               plotlyOutput("plot2")
+               column(width = 9,
+                      plotOutput("plot2", height = 350,
+                      click = "plot2_click")
+               ),
+               column(width = 7,
+                      h4("Points near Click"),
+                      verbatimTextOutput("click_info2"))
              )
            )
            )
@@ -152,22 +190,38 @@ ui <- navbarPage(
 )
 
 
-# Define server logic required to draw a histogram
+
+# Define server logic used to generate each of the plots and the corresponding click output provided when the user clicks
+## on any of the points.
+
 server <- function(input, output) {
-  output$Plot <- renderPlotly({
+  output$Plot <- renderPlot({
     
-      # Generating the first plot that I listed the inputs for above
+      # Generating the first plot that I listed the inputs for above and including a minor line of best fit.
+    
     pgroups %>% 
       ggplot(aes_string(x = input$xcol, y = input$ycol)) + geom_point() + geom_smooth(method = "lm")
              }
   )
+  
+  ## This is the code I used to print out the data point that the user clicks on.
+  
+  output$click_info <- renderPrint({
+    nearPoints(pgroups, input$Plot_click, addDist = TRUE)
+  })
 
-  output$plot2 <- renderPlotly({
+  ## The code used to generate the plot for the draft and the value derived from those players. I am filtering on position here
+  ## even though I could not achieve my end goal of being able to filter by both team and position.
+  
+  output$plot2 <- renderPlot({
     draft_value %>%
       filter(Pos == input$position) %>%
       ggplot(aes_string(x = input$xselect, y = input$yselect)) + geom_point() + 
       geom_smooth(method = "lm", color = "black", show.legend = TRUE)
       #stat_smooth(geom = "text", method = "lm", hjust = 0, parse = TRUE)
+  })
+  output$click_info2 <- renderPrint({
+    nearPoints(draft_value, input$plot2_click, addDist = TRUE)
   })
    }
 
